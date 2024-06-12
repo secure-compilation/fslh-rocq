8d4be8c8e959d514a0bfe7e4096af54ced550fb7
Rob Blanco <roberto.blanco@mpi-sp.org>
2024-06-12 08:30:48 +0200

    Getting quickChick tool to run
    With Coq 8.19.1 and QuickChick 2.0.2:
    
     - No parser problems!
    
     - The tool does not seem to like namespaces, need to do something about
       this.
    
     - There is still a build error at the end, which should be inspected as
       soon as we have some mutants running (can we use the [QuickChick]
       commands as they are inside special comments?)

--- mutation-testing/TestingSpecCT.v
+++ mutation-testing/TestingSpecCT.v
@@ -3,7 +3,7 @@
 (* TERSE: HIDEFROMHTML *)
 Set Warnings "-notation-overridden,-parsing,-deprecated-hint-without-locality".
 From Coq Require Import Strings.String.
-From SECF Require Import Maps.
+(* From SECF *) Require Import Maps.
 From Coq Require Import Bool.Bool.
 From Coq Require Import Arith.Arith.
 From Coq Require Import Arith.EqNat.
